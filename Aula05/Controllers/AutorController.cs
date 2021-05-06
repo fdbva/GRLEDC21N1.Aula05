3b13e4ade59da50b4f7abbbed423ef54121d2cd3
fdbva <villela.felipe@gmail.com>
2021-05-05 22:18:18 -0300

    Add project files.

---
+++ Aula05/Controllers/AutorController.cs
@@ -0,0 +1,35 @@
+﻿using Aula05.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aula05.Controllers
+{
+    public class AutorController : Controller
+    {
+        public IActionResult Index()
+        {
+            var autorModel = new AutorModel();
+            autorModel.Id = 1;
+            autorModel.Nome = "Felipe Andrade";
+
+            return View(autorModel);
+        }
+
+        public IActionResult Index2()
+        {
+            var autorModel = new AutorModel();
+            autorModel.Id = 1;
+            autorModel.Nome = "Felipe Andrade";
+
+            return View(autorModel);
+        }
+
+        public IActionResult Livro()
+        {
+            var autorModel = new AutorModel();
+            autorModel.Id = 1;
+            autorModel.Nome = "Felipe Andrade";
+
+            return View("Index1", autorModel);
+        }
+    }
+}
